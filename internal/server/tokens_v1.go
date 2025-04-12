e94399c808caeb1977c02abd3245b0c4f084f036
ashwinprasad <d3fkonmusic@gmail.com>
2025-04-12 14:57:20 +0530

    feat: token pairs

--- internal/server/tokens_v1.go
+++ internal/server/tokens_v1.go
@@ -1,6 +1,28 @@
 package server
 
-func (s *Server) getTokensHandler() {
+import "github.com/labstack/echo/v4"
+
+type token struct {
+	Name     string
+	FullName string
+	Address  string
+	Decimals uint32
+}
+
+type tokenPair struct {
+	TokenA token
+	TokenB token
+	Name   string
+}
+
+func (s *Server) getTokensHandler(ctx echo.Context) error {
 	wS := "0x5A91D3042b71A92f6757Fa937763D03Cc65ED8BC"
 	RiftX := "0x2b258Ba5F33A72DcEf2E70A11B75Dd0eF0730942"
+	pair1 := tokenPair{
+		TokenA: token{FullName: "Rift (ODX)", Name: "Rift.X", Address: RiftX, Decimals: 18},
+		TokenB: token{FullName: "Wrapped Sonic", Name: "wS", Address: wS, Decimals: 18},
+		Name:   "Rift.X/wS",
+	}
+	pairs := []tokenPair{pair1}
+	return ctx.JSON(200, pairs)
 }
